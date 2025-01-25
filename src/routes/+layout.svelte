3b2520e9a6178d6d69aece779aedac895a000e04
Harshit Tomar <harshitt345@gmail.com>
2025-01-25 16:09:16 +0530

    Add navigation sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,38 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	import { Settings, House, Logs, ChartNoAxesColumn, ChartSpline } from 'lucide-svelte';
 </script>
 
-{@render children()}
+<div class="cont flex h-[100vh]">
+	<Navigation.Rail>
+		{#snippet header()}
+			<div
+				class="icon flex aspect-square w-full flex-col items-center justify-center gap-1 rounded-container p-2 hover:preset-filled-surface-50-950"
+			>
+				DePWN
+			</div>
+		{/snippet}
+		{#snippet tiles()}
+			<Navigation.Tile id="0" label="Dash" href="/">
+				<House />
+			</Navigation.Tile>
+			<Navigation.Tile id="1" label="Logs" href="/logs">
+				<Logs />
+			</Navigation.Tile>
+			<Navigation.Tile id="2" label="Statistics" href="/stats">
+				<ChartSpline />
+			</Navigation.Tile>
+		{/snippet}
+		{#snippet footer()}
+			<Navigation.Tile labelExpanded="Settings" href="/settings" title="settings">
+				<Settings />
+			</Navigation.Tile>
+		{/snippet}
+	</Navigation.Rail>
+	<main class="cont grid h-[100vh] w-full place-items-center">
+		{@render children()}
+	</main>
+</div>

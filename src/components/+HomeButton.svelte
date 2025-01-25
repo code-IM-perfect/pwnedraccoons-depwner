bf4dbd00579f3a01094826ba3d549b12bd23620e
Harshit Tomar <harshitt345@gmail.com>
2025-01-25 16:07:08 +0530

    Make card component

---
+++ src/components/+HomeButton.svelte
@@ -0,0 +1,9 @@
+<script>
+	let { children } = $props();
+</script>
+
+<button
+	class="card grid place-items-center border-[1px] p-4 border-surface-200-800 preset-filled-surface-100-900"
+>
+	{@render children()}
+</button>

bace53e4c2c8e63aed592bef4a04c564a2fc4307
Harshit Tomar <harshitt345@gmail.com>
2025-01-26 11:29:27 +0530

    Dashboard card styling

--- src/components/+HomeButton.svelte
+++ src/components/+HomeButton.svelte
@@ -1,9 +1,24 @@
 <script>
-	let { children } = $props();
+	let { children, col = '', row = '' } = $props();
 </script>
 
 <button
-	class="card grid place-items-center border-[1px] p-4 border-surface-200-800 preset-filled-surface-100-900"
+	class="homeCard card border-[1px] border-surface-200-800 preset-filled-surface-100-900"
+	style="grid-row:{row};grid-column:{col};"
 >
-	{@render children()}
+	<div class="cardInner">
+		{@render children()}
+	</div>
 </button>
+
+<style>
+	.homeCard {
+		padding: min(5vh, 5vw);
+		/* font-size: 2vh; */
+	}
+	.homeCard :global(.lucide-icon) {
+		--icon-size: min(15vh, 12vw);
+		height: var(--icon-size);
+		width: var(--icon-size);
+	}
+</style>

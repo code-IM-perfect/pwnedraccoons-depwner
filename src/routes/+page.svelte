8c1b08f1ccbccb9cc5120f8b36590f48de84fe83
Harshit Tomar <harshitt345@gmail.com>
2025-01-26 11:26:52 +0530

    Improve dashboard styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
 	<title>DePWNer</title>
 </svelte:head>
 
+<div class="dashContainer grid grid-cols-3 text-center">
 	<HomeButton>
 		<RefreshCcw />
 		<h3>Run a Scan</h3>
@@ -30,3 +31,9 @@
 		<h3>Status</h3>
 	</HomeButton>
 </div>
+
+<style>
+	.dashContainer {
+		gap: min(2vh, 2vw);
+	}
+</style>
